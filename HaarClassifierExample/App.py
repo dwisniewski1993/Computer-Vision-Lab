66672377657db9cbc688c586494e076992a92f9f
XIIIvs <xiii_1994@wp.pl>
2018-01-15 20:51:13 +0100

    Working adding glasses on faces

--- HaarClassifierExample/App.py
+++ HaarClassifierExample/App.py
@@ -3,17 +3,28 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('lena.jpg')
+img = cv2.imread('faces.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-glasses = cv2.imread('glasses2.png', cv2.IMREAD_UNCHANGED)
+# load glasses with alpha
+glasses = cv2.imread('glasses.png', cv2.IMREAD_UNCHANGED)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-for face in faces:
-    (x, y, w, h) = face
+for (x, y, w, h) in faces:
+    # scale glasses and draw rectangle where is face
     scaled_glasses = cv2.resize(glasses, (w, h))
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 1)
+
+    # alpha variables
+    alpha_glasses = scaled_glasses[:, :, 3] / 255.0
+    alpha_img = 1.0 - alpha_glasses
+
+    # add alpha channel for original image
+    for c in range(0, 3):
+        img[y:y+h, x:x+w, c] = (alpha_glasses * scaled_glasses[:, :, c] +
+                                alpha_img * img[y:y+h, x:x+w, c])
+
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     # eyes = eye_cascade.detectMultiScale(roi_gray)
@@ -21,6 +32,6 @@ for face in faces:
     #     cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
 
-cv2.imshow('img', img)
+cv2.imshow('GlassesMakerBaker', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
