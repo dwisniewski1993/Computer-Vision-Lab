be9477f9aaf7b08bff06aaebc368eeae425bb482
XIIIvs <xiii_1994@wp.pl>
2018-01-25 11:43:09 +0100

    Working cv image in pygame surface

--- HaarClassifierExample/AppWithInterface.py
+++ HaarClassifierExample/AppWithInterface.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy
 from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
 from sys import exit
 from cvimage import ImageOpenCV
@@ -11,15 +12,20 @@ RESOLUTION_X = 800
 RESOLUTION_Y = 600
 
 
+def cv2pygame(image):
+    return pygame.image.frombuffer(image.tostring(), image.shape[1::-1], "RGB")
+
+
 class InterfaceModule:
     def __init__(self):
         self.main_display = pygame.display.set_mode((RESOLUTION_X, RESOLUTION_Y), MODE)
+        self.image = ImageOpenCV("lena.jpg", 400, 300)
         # TODO: Create Box with all masks
         # TODO: Create Box with all images in app directory
 
     def event(self):
         for event in pygame.event.get():
-            print(event)
+            # print(event)
 
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 print("Exiting Program")
@@ -28,8 +34,10 @@ class InterfaceModule:
     def run(self):
         while True:
             self.main_display.fill((24, 131, 215))
+            self.main_display.blit(cv2pygame(self.image.to_draw(1)), (100, 100))
             self.event()
             pygame.display.update()
+            # exit()
 
 
 if __name__ == "__main__":
--- HaarClassifierExample/cvimage.py
+++ HaarClassifierExample/cvimage.py
@@ -2,28 +2,32 @@ import copy
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 SCALE_FACTOR = 1.3
 MIN_NEIGHBORS = 5
 FACE_COLOR = (255, 0, 0)  # B G R
 EYES_COLOR = (0, 255, 0)  # B G R
 BORDER_SIZE = 1
+INTERPOLATION_MODE = cv2.INTER_LINEAR
 
 
 class ImageOpenCV:
-    def __init__(self, name):
+    def __init__(self, name, width, height):
+        self.width = width
+        self.height = height
         self.file_name = str()
         self.image = None
         self.image_wth_borders = None
         self.gray = None
         self.faces = None
-        self.eyes = None
+        # self.eyes = None
         self.load_image(name)
 
     def load_image(self, name):
         self.file_name = name
-        self.image = cv2.imread(name)
+        image = cv2.imread(name)
+        self.image = cv2.resize(image, (self.width, self.height), interpolation=INTERPOLATION_MODE)
         self.image_wth_borders = copy.deepcopy(self.image)
         self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.detect_faces_and_eyes()
@@ -33,8 +37,19 @@ class ImageOpenCV:
         for (x, y, width, height) in self.faces:
             cv2.rectangle(self.image_wth_borders, (x, y), (x+width, y+height), FACE_COLOR, BORDER_SIZE)
 
-            roi_gray = self.gray[y:y+height, x:x+width]
-            roi_color = self.image_wth_borders[y:y+height, x:x+width]
-            self.eyes = eye_cascade.detectMultiScale(roi_gray)
-            for (x_eye, y_eye, width_eye, height_eye) in self.eyes:
-                cv2.rectangle(roi_color, (x_eye, y_eye), (x_eye+width_eye, y_eye+height_eye), EYES_COLOR, BORDER_SIZE)
+            # roi_gray = self.gray[y:y+height, x:x+width]
+            # roi_color = self.image_wth_borders[y:y+height, x:x+width]
+            # self.eyes = eye_cascade.detectMultiScale(roi_gray)
+            # for (x_eye, y_eye, width_eye, height_eye) in self.eyes:
+            #     cv2.rectangle(roi_color, (x_eye, y_eye), (x_eye+width_eye, y_eye+height_eye), EYES_COLOR, BORDER_SIZE)
+
+    def to_draw(self, flag=0):
+        """
+        default - image scaled without borders
+        flag 1  - image scaled with borders
+        """
+        if flag == 1:
+            image = self.image_wth_borders
+        else:
+            image = self.image
+        return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
